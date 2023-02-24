0e01aefe69c3fc4badbf34cc91dc4c23e93c0d69
atforche <atforche@gmail.com>
2023-02-23 20:51:13 -0500

    Fixed bug that preventing viewing different budgets in the Spending Graph module

--- modules/spendingGraphServer.R
+++ modules/spendingGraphServer.R
@@ -45,10 +45,11 @@ spending_graph_server <- function(id, select_month)
         # If the user selected a budget, only show that budget
         if (input$select_budget != "All") 
         {
+          browser()
           transaction_table <- transaction_table %>%
-            filter(Name == input$select_budget())
+            filter(Category == input$select_budget)
           previous_transactions <- previous_transactions %>%
-            filter(Name == input$select_budget())
+            filter(Category == input$select_budget)
         }
         
         # Calculate the running totals
